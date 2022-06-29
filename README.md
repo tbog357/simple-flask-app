aff77ba97dac4278ed4d112cf542a2ee0aeffa69
tbog357 <hahuydung357@gmail.com>
2022-06-29 22:15:12 +0700

    Ref: export conda env, trigger updated_at col, remove logs

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-## Host:
+## Host Information:
 - Ubuntu 22.04
 - docker-compose version 1.29.2, build 5becea4c
 
 ## Steps to run:
+- conda env create --file environment.yml -n <env_name>
+- conda activate <env_name>
 - mkdir docker_volume
+- mkdir logs
+- touch logs/app.log
 - sudo docker-compose up
 
-## Postman collections:
+## Postman collection:
 https://www.getpostman.com/collections/26834749f5bc00da35f2
--- create_tables.sql
+++ create_tables.sql
@@ -2,14 +2,33 @@ CREATE DATABASE "appdb"
    WITH OWNER "postgres"
    ENCODING 'UTF8';
 
+-- chose db
 \c appdb;
 
+-- trigger for updated_at column
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- create a toy customer table 
 CREATE TABLE IF NOT EXISTS appdb.public.customer(
    id           SERIAL PRIMARY KEY NOT NULL,
-   email        VARCHAR(50) UNIQUE NOT NULL,
+   email        VARCHAR(50) NOT NULL,
    phone        VARCHAR(50),
    address      VARCHAR(50),
    name         VARCHAR(50) NOT NULL,
    status       VARCHAR(50) NOT NULL,
-   created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   updated_at   TIMESTAMP,
+   CONSTRAINT unique_customer_email UNIQUE (email)
 );
+
+-- set updated_at trigger for customer table
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON customer
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
